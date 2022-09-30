a72d521cb2a64838a852f68c62badcb540cad16c
Alex Barakhov <abarakhov@gmail.com>
2022-09-30 08:54:10 +0200

    added counter

--- script.js
+++ script.js
@@ -3,22 +3,57 @@ Splitting();
 let reg = document.querySelector("body");
 let img = document.querySelector(".screen");
 let cnt = document.querySelector(".container");
-let btn = document.querySelector(".empty");
+let emptyButton = document.querySelector(".empty");
+let sumButton = document.querySelector(".sum")
+let restButton = document.querySelector(".rest")
+let extraButton = document.querySelectorAll(".extra")
+
+
+let counter = 1;
 
 function report() { 
+ extraButton.forEach(el => el.style.color = 'transparent')
   html2canvas(reg, {
     onrendered: function(canvas) {
       let pngUrl = canvas.toDataURL();
       img.src = pngUrl; 
       cnt.classList.add('hover')
-      btn.classList.remove('hidden')
+      emptyButton.classList.remove('hidden')
 
     },
   });
 }
 
 function empty() {
+  extraButton.forEach(el => el.style.color = '#a8a8a8')
   img.setAttribute('src', '');
   cnt.classList.remove('hover')  
-  btn.classList.add('hidden')
+  emptyButton.classList.add('hidden')
+}
+
+function handleSum() {
+
+  if(counter <= 7) {
+    counter += 1;
+    restButton.removeAttribute("disabled");
+  } else {
+    sumButton.setAttribute("disabled", ""); 
+  }
+
+  let convesion = 9 - counter;
+
+  document.querySelector('[data-'+convesion+']').classList.remove('hide');
+
+}
+
+function handleRest() {
+  if(counter <= 8 && counter >= 2) {
+    counter -= 1;
+    sumButton.removeAttribute("disabled");
+  } else {
+    restButton.setAttribute("disabled", ""); 
+  }
+
+  let convesion = 8 - counter;
+  document.querySelector('[data-'+convesion+']').classList.add('hide');
 }
